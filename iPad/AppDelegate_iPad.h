25fecbf5e3dab3c25380a608594cdd6d21857319
David M. Syzdek <syzdek@bindlebinaries.com>
2010-11-22 13:47:02 -0900

    Creating initial Xcode project for Universal iOS App.

---
+++ iPad/AppDelegate_iPad.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate_iPad.h
+//  iOSPorts
+//
+//  Created by David Syzdek on 11/22/10.
+//  Copyright 2010 David M. Syzdek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate_iPad : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ iPhone/AppDelegate_iPhone.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate_iPhone.h
+//  iOSPorts
+//
+//  Created by David Syzdek on 11/22/10.
+//  Copyright 2010 David M. Syzdek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate_iPhone : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
